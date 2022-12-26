80e92e1e5ad51485d333337998e4d7509205b2cc
Hamidreza Niazi <hamidrezaniazi@yahoo.com>
2022-12-27 00:50:01 +0100

    add faker generator to the feature test case

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,11 +2,21 @@
 
 namespace Hamidrezaniazi\Pecs\Tests;
 
+use Faker\Factory;
+use Faker\Generator;
 use Illuminate\Support\Arr;
 use PHPUnit\Framework\TestCase as BaseTestCase;
 
 class TestCase extends BaseTestCase
 {
+    protected Generator $faker;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->faker = Factory::create();
+    }
+
     protected function assertEqualsMultidimensionalCanonicalizing(array $expected, array $actual): void
     {
         $expected = Arr::dot($expected);
