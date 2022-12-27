592009f6c306d1990bd8f4a417f24f866d0241e0
Hamidreza Niazi <hamidrezaniazi@yahoo.com>
2022-12-27 03:23:09 +0100

    add percent class

---
+++ src/Properties/Percent.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Hamidrezaniazi\Pecs\Properties;
+
+use Exception;
+
+class Percent
+{
+    public function __construct(
+        public readonly float $value,
+    ) {
+        if ($this->value > 100 || $this->value < 0) {
+            throw new Exception('invalid percentage value! it should be between 0 to 100');
+        }
+    }
+}
---
+++ tests/Unit/Properties/PercentTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Hamidrezaniazi\Pecs\Tests\Unit\Properties;
+
+use Hamidrezaniazi\Pecs\Properties\Percent;
+use PHPUnit\Framework\TestCase;
+
+class PercentTest extends TestCase
+{
+    public function testItShouldValidateInput(): void
+    {
+        $this->expectExceptionMessage('invalid percentage value! it should be between 0 to 100');
+
+        $value = rand(101, 1001);
+        new Percent($value);
+    }
+
+    public function testItCanCastItselfToString(): void
+    {
+        $value = rand(0, 100);
+        $percent = new Percent($value);
+
+        $this->assertEquals($value, $percent->value);
+    }
+}
