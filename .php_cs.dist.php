37649c740d0b5cfdcfe95d561fcba28c9f6bc5ff
Hamidreza Niazi <hamidrezaniazi@yahoo.com>
2023-04-20 23:04:51 +0200

    add php cs package

---
+++ .php_cs.dist.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$finder = PhpCsFixer\Finder::create()
+    ->exclude('vendor')
+    ->in(__DIR__);
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@PSR2' => true,
+        'array_indentation' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'no_unused_imports' => true,
+        'ordered_imports' => true,
+    ])
+    ->setFinder($finder);
